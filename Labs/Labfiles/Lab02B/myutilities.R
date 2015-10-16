2370903c9cf21c95b99b9829d97e26ded3f12f07
GraemeMalcolm <gmalc@microsoft.com>
2015-10-16 15:32:20 +0100

    deleted:    Labs/Labfiles/Lab01.zip 	deleted:    Labs/Labfiles/Lab01/Bike Rental Code.txt 	deleted:    Labs/Labfiles/Lab02A/admissions_mapping.csv 	deleted:    Labs/Labfiles/Lab02A/diabetic_data.csv

--- Labs/Labfiles/Lab02B/myutilities.R
+++
@@ -1,5 +0,0 @@
-round2 <- function(x){
-  ## Round values to 2 decimal places
-  round(x, 2)
-}
-
--- Labs/Labfiles/Lab02B/myutilities.py
+++
@@ -1,5 +0,0 @@
-def round2(df):
-## Round values to 2 decimal places
-  import numpy as np
-  return df.apply(lambda x: np.round(x, 2))
-
